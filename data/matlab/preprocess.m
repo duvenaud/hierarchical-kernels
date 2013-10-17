508bda88be0c450e3a3eae6ee2c1ee1255d2f766
David Duvenaud <dkd23@cam.ac.uk>
2013-10-17 19:12:15 +0100

    Combined datasets

---
+++ data/matlab/preprocess.m
@@ -0,0 +1,58 @@
+% Combine all datasets.
+%
+% David Duvenaud
+% Oct 2013.
+
+
+% Fix the seed of the random generators.
+seed = 0;
+randn('state',seed);
+rand('state',seed);
+
+
+datadir = '';
+datafiles = dir([ datadir, '*.mat']);
+outdir = '';
+
+% Concatenate the data.
+for dataset_ix = 1:length(datafiles)
+    cur_dataset_name = [ datadir, datafiles(dataset_ix).name];
+    cur_dataset = load(cur_dataset_name);
+    
+    if dataset_ix == 1
+        X = cur_dataset.X;
+        y = cur_dataset.y;
+        D = cur_dataset.D;
+    else
+        X = [X; cur_dataset.X];
+        y = [y; cur_dataset.y];
+        D = [D; cur_dataset.D];
+    end
+end
+
+save('concatenated.mat', 'X', 'y', 'D' );
+orig_y = y;
+y = log(y);
+save('concatenated_log.mat', 'X', 'y', 'D' );
+y = orig_y;
+
+
+orig_X = X;
+X(D == 0) = NaN;
+save('concatenated_nan.mat', 'X', 'y', 'D' );
+
+orig_y = y;
+y = log(y);
+save('concatenated_nan_log.mat', 'X', 'y', 'D' );
+y = orig_y;
+
+
+
+% Some exploratory data analysis
+[N,dims] = size(X)
+
+for d = 1:dims
+    clf; plot(squeeze(X(:, d)), y, '.');
+    title(d);
+    pause;
+end
