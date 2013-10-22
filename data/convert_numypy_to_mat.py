3ae19c0b25a8bf59fcf7f175b705c5d5cf950438
David Duvenaud <dkd23@cam.ac.uk>
2013-10-22 15:55:25 +0100

    Re-processed fixed dataset

--- data/convert_numypy_to_mat.py
+++ data/convert_numypy_to_mat.py
@@ -8,9 +8,10 @@ import numpy as np
 import os
 import scipy.io
 
-numpy_dir = 'numpy'
+numpy_dir = 'numpy/'
 
 for file in os.listdir(numpy_dir):
-	data = np.load(numpy_dir + '/' + file)
-	scipy.io.savemat( 'matlab/' + file.rsplit( ".", 1 )[ 0 ] + '.mat', data)
+	data = np.load(numpy_dir + file)
+	print data
+	scipy.io.savemat( 'matlab/arcnet/' + file.rsplit( ".", 1 )[ 0 ] + '.mat', data)
 
